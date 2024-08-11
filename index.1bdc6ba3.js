492b3de20e5a448c74d5a7bec33675b05854b8c6
Mainaiev <farid.mainaiev@proton.me>
2024-08-11 03:38:04 +0300

    make build

---
+++ index.1bdc6ba3.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall");t.addEventListener("click",function(){var i=t.getBoundingClientRect(),n=e.getBoundingClientRect(),l=(i.height-n.height)/2,c=(i.width-n.width)/2;e.style.top=l+"px",e.style.left=c+"px"});
+//# sourceMappingURL=index.1bdc6ba3.js.map
